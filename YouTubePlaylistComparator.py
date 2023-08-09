f6974d64f9ee9aeb9fa7dba29cf054e95968001a
Youhua Chen <youhuawesome@gmail.com>
2023-08-08 22:05:48 -0700

    added the feature to detect duplicates

--- YouTubePlaylistComparator.py
+++ YouTubePlaylistComparator.py
@@ -29,12 +29,19 @@ def getplaylist(apibuild, id):
         nextToken = None
 
     playlist_videos = set()
+    playlist_duplicates = {}
     x = 0
     while x < 1000:
 
         for i in response["items"]:
             #response_1["items"] is a list of playlistItem, which is a json dictionary.
-            playlist_videos.add(i["snippet"]["title"])
+            if i["snippet"]["title"] not in playlist_videos:
+                playlist_videos.add(i["snippet"]["title"])
+            else:
+                if i["snippet"]["title"] not in playlist_duplicates:
+                    playlist_duplicates[i["snippet"]["title"]] = 1
+                else:
+                    playlist_duplicates[i["snippet"]["title"]] = playlist_duplicates[i["snippet"]["title"]] + 1
 
         if nextToken == None:
             break
@@ -55,7 +62,7 @@ def getplaylist(apibuild, id):
         
         x = x + 1
     
-    return playlist_videos
+    return playlist_videos, playlist_duplicates
 
 #returns: 3 sorted lists of songs.
 def playlistCompare(a , b):
@@ -84,6 +91,20 @@ def playlistCompare(a , b):
     songsC.sort()
     
     return songsA, songsB, songsC
+
+
+def printDuplicates(a, playlistname):
+    print("playlist " + playlistname + " has the following duplicates")
+    print("")
+
+    duplicates = list(a.keys())
+    duplicates.sort()
+
+    for i in duplicates:
+        print(i + "     " + " --> # of duplicates: " + str(a[i]))
+
+    print("")
+    print("")
 
 def printSizeofPlaylists(pl1n, pl2n, s1, s2):
     print("The size of playlist " + pl1n + " is " + str(s1))
@@ -116,7 +137,7 @@ def main():
     ###################
     ##### API key #####
     ###################
-    DEVELOPER_KEY = "PUT YOUR DEVELOPER KEY HERE"
+    DEVELOPER_KEY = "YOUR DEVELOPER KEY HERE"
 
 
     ###########################
@@ -143,8 +164,8 @@ def main():
     #############################
     ##### getting playlists #####
     #############################
-    playlist1 = getplaylist(youtube, playlist1_id)
-    playlist2 = getplaylist(youtube, playlist2_id)
+    playlist1, duplicates_1 =  getplaylist(youtube, playlist1_id)
+    playlist2, duplicates_2 = getplaylist(youtube, playlist2_id)
 
 
     ####################################################################
@@ -158,10 +179,12 @@ def main():
     ####################################
     printSizeofPlaylists(playlist1_name, playlist2_name, size1, size2)
 
+    printDuplicates(duplicates_1, playlist1_name)
+    printDuplicates(duplicates_2, playlist2_name)
+
     printPlaylists(songsBoth, "songs in both playlists " + playlist1_name + " and " + playlist2_name + " :")
 
     printPlaylists(songs1, "songs in playlist " + playlist1_name + ", but not in playlist " + playlist2_name + " :")
-
     printPlaylists(songs2, "songs in playlist " + playlist2_name + ", but not in playlist " + playlist1_name + " :")
 
 
